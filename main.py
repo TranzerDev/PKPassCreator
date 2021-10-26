433b6319f0fc6564a1010355ee57216e7624fee9
kamaal111 <kamaal.f1@gmail.com>
2021-10-26 22:57:30 +0200

    Dynamically getting argument values

--- main.py
+++ main.py
@@ -3,16 +3,12 @@ import hashlib
 import os
 import sys
 import shutil
-from typing import Callable, Optional
+from typing import Callable, Optional, TypedDict
 from zipfile import ZipFile
 
 from pass_dict import pass_dict
 
-KEY_PASSWORD = "password"
-WWDR_PATH = "WWDR.pem"
-CERTIFICATE_PATH = "certificates.p12"
-PASS_TYPE_IDENTIFIER = "com.apple.application-password"
-TEAM_IDENTIFIER = "2V9Z6R8X2R"
+
 PK_PASS_NAME = "Generic"
 OPENSSL_APP = "openssl"
 
@@ -30,6 +26,7 @@ SUPPORTED_ASSET_FILES = [
     "thumbnail.png",
     "thumbnail@2x.png",
 ]
+
 
 def with_clean_up(func: Callable):
     def wrapper(*args, **kwargs):
@@ -51,11 +48,22 @@ def with_clean_up(func: Callable):
                     os.remove(asset_file)
     return wrapper
 
+
 @with_clean_up
 def main():
+    arguments = parse_arguments()
+
+    pass_type_identifier = arguments["pass_type_identifier"]
+    if pass_type_identifier is "":
+        raise Exception("pass type identifier is empty")
+
+    team_identifier = arguments["team_identifier"]
+    if team_identifier is "":
+        raise Exception("team identifier is empty")
+
     pass_dict_copy = create_pass_dict(
-        pass_type_identifier=PASS_TYPE_IDENTIFIER,
-        team_identifier=TEAM_IDENTIFIER,
+        pass_type_identifier=pass_type_identifier,
+        team_identifier=team_identifier,
     )
     pass_dict_json = json.dumps(pass_dict_copy, indent=2)
     with open("pass.json", "w") as f:
@@ -63,39 +71,25 @@ def main():
 
     create_manifest_json(asset_path=f"{PK_PASS_NAME}.pass")
 
-    certificate_password: Optional[str] = None
-    skip_next_value = False
-    for index, arg in enumerate(sys.argv[1:]):
-        if skip_next_value:
-            skip_next_value = False
-            continue
-        match arg:
-            case "--cert-pass":
-                if index + 1 < len(sys.argv[index:]):
-                    certificate_password = sys.argv[index + 2]
-                    skip_next_value = True
-                else:
-                    print("--cert-pass requires a password")
-
-    if not certificate_password:
-        certificate_password = input("what is the password of the provided certificate?\n")
-        return
+    certificate_password = arguments["certificate_password"]
+    certificate_path = arguments["certificate_path"]
 
     #  Create pass certificate
-    os_code = os.system(f"{OPENSSL_APP} pkcs12 -in {CERTIFICATE_PATH} -clcerts -nokeys -out passcertificate.pem -passin pass:{certificate_password}")
+    os_code = os.system(f"{OPENSSL_APP} pkcs12 -in {certificate_path} -clcerts -nokeys -out passcertificate.pem -passin pass:{certificate_password}")
     if os_code != 0:
-        print("Error creating pass certificate")
-        return
+        raise Exception("could not create pass certificate")
+
+    key_password = arguments["key_password"]
     # Create pass key
-    os_code = os.system(f"{OPENSSL_APP} pkcs12 -in {CERTIFICATE_PATH} -nocerts -out passkey.pem -passin pass:{certificate_password} -passout pass:{KEY_PASSWORD}")
+    os_code = os.system(f"{OPENSSL_APP} pkcs12 -in {certificate_path} -nocerts -out passkey.pem -passin pass:{certificate_password} -passout pass:{key_password}")
     if os_code != 0:
-        print("Error creating pass key")
-        return
+        raise Exception("could not create pass key")
+
+    wwdr_path = arguments["wwdr_path"]
     # Create signature
-    os_code = os.system(f"{OPENSSL_APP} smime -binary -sign -certfile {WWDR_PATH} -signer passcertificate.pem -inkey passkey.pem -in manifest.json -out signature -outform DER -passin pass:{KEY_PASSWORD}")
+    os_code = os.system(f"{OPENSSL_APP} smime -binary -sign -certfile {wwdr_path} -signer passcertificate.pem -inkey passkey.pem -in manifest.json -out signature -outform DER -passin pass:{key_password}")
     if os_code != 0:
-        print("Error creating signature")
-        return
+        raise Exception("could not create signature")
 
     asset_files_to_delete = [
         "passkey.pem",
@@ -122,9 +116,85 @@ def main():
 
     os_code = os.system(f"open {PK_PASS_NAME}.pkpass")
     if os_code != 0:
-        print("Error opening pass")
-        return
+        raise Exception("could not open pkpass")
 
+
+class Arguments(TypedDict):
+    certificate_password: str
+    key_password: str
+    pass_type_identifier: str
+    team_identifier: str
+    certificate_path: str
+    wwdr_path: str
+
+def parse_arguments() -> "Arguments":
+    certificate_password: Optional[str] = None
+    key_password: Optional[str] = None
+    pass_type_identifier: Optional[str] = None
+    team_identifier: Optional[str] = None
+    certificate_path: Optional[str] = None
+    wwdr_path: Optional[str] = None
+
+    skip_next_value = False
+    for index, arg in enumerate(sys.argv[1:]):
+        if skip_next_value:
+            skip_next_value = False
+            continue
+
+        next_value_is_getable = index + 1 < len(sys.argv[index:])
+        next_value_index = index + 2
+        match arg:
+            case "--certificate-password":
+                if next_value_is_getable:
+                    certificate_password = sys.argv[next_value_index]
+                    skip_next_value = True
+            case "--pass-key-password":
+                if next_value_is_getable:
+                    key_password = sys.argv[next_value_index]
+                    skip_next_value = True
+            case "--pass-type-identifier":
+                if next_value_is_getable:
+                    pass_type_identifier = sys.argv[next_value_index]
+                    skip_next_value = True
+            case "--team-identifier":
+                if next_value_is_getable:
+                    team_identifier = sys.argv[next_value_index]
+                    skip_next_value = True
+            case "--certificate-path":
+                if next_value_is_getable:
+                    certificate_path = sys.argv[next_value_index]
+                    skip_next_value = True
+            case "--wwdr-path":
+                if next_value_is_getable:
+                    wwdr_path = sys.argv[next_value_index]
+                    skip_next_value = True
+
+    if certificate_password is None:
+        certificate_password = input("what is the password of the provided certificate?\n")
+
+    if key_password is None:
+        key_password = input("provide a password for your passkey.pem\n")
+
+    if pass_type_identifier is None:
+        pass_type_identifier = input("provide a pass type identifier\n")
+
+    if team_identifier is None:
+        team_identifier = input("provide a team identifier\n")
+
+    if certificate_path is None:
+        certificate_path = input("provide the path to your certificate\n")
+
+    if wwdr_path is None:
+        wwdr_path = input("provide the path to your WWDR certificate\n")
+
+    return {
+        "certificate_password": certificate_password,
+        "key_password": key_password,
+        "pass_type_identifier": pass_type_identifier,
+        "team_identifier": team_identifier,
+        "certificate_path": certificate_path,
+        "wwdr_path": wwdr_path,
+    }
 
 
 def create_manifest_json(asset_path: str):
