b06305ac892ee0141cfa1aff50d3b03622e13742
kamaal111 <kamaal.f1@gmail.com>
2021-10-26 22:22:59 +0200

    Handling command line issues

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import hashlib
 import os
 import sys
 import shutil
-from typing import Optional
+from typing import Callable, Optional
 from zipfile import ZipFile
 
 from pass_dict import pass_dict
@@ -31,7 +31,27 @@ SUPPORTED_ASSET_FILES = [
     "thumbnail@2x.png",
 ]
 
+def with_clean_up(func: Callable):
+    def wrapper(*args, **kwargs):
+        try:
+            func(*args, **kwargs)
+        except Exception as e:
+            print(e)
+        finally:
+            generated_files = [
+                "pass.json",
+                "manifest.json",
+                "passcertificate.pem",
+                "passkey.pem",
+                "signature",
+            ]
 
+            for asset_file in SUPPORTED_ASSET_FILES + generated_files:
+                if os.path.exists(asset_file):
+                    os.remove(asset_file)
+    return wrapper
+
+@with_clean_up
 def main():
     pass_dict_copy = create_pass_dict(
         pass_type_identifier=PASS_TYPE_IDENTIFIER,
@@ -44,30 +64,38 @@ def main():
     create_manifest_json(asset_path=f"{PK_PASS_NAME}.pass")
 
     certificate_password: Optional[str] = None
-    skip_value = False
+    skip_next_value = False
     for index, arg in enumerate(sys.argv[1:]):
-        if skip_value:
-            skip_value = False
+        if skip_next_value:
+            skip_next_value = False
             continue
         match arg:
             case "--cert-pass":
                 if index + 1 < len(sys.argv[index:]):
                     certificate_password = sys.argv[index + 2]
-                    skip_value = True
+                    skip_next_value = True
                 else:
                     print("--cert-pass requires a password")
-                    return
 
-    if certificate_password is None:
-        print("No certificate password provided")
+    if not certificate_password:
+        certificate_password = input("what is the password of the provided certificate?\n")
         return
 
     #  Create pass certificate
-    os.system(f"{OPENSSL_APP} pkcs12 -in {CERTIFICATE_PATH} -clcerts -nokeys -out passcertificate.pem -passin pass:{certificate_password}")
+    os_code = os.system(f"{OPENSSL_APP} pkcs12 -in {CERTIFICATE_PATH} -clcerts -nokeys -out passcertificate.pem -passin pass:{certificate_password}")
+    if os_code != 0:
+        print("Error creating pass certificate")
+        return
     # Create pass key
-    os.system(f"{OPENSSL_APP} pkcs12 -in {CERTIFICATE_PATH} -nocerts -out passkey.pem -passin pass:{certificate_password} -passout pass:{KEY_PASSWORD}")
+    os_code = os.system(f"{OPENSSL_APP} pkcs12 -in {CERTIFICATE_PATH} -nocerts -out passkey.pem -passin pass:{certificate_password} -passout pass:{KEY_PASSWORD}")
+    if os_code != 0:
+        print("Error creating pass key")
+        return
     # Create signature
-    os.system(f"{OPENSSL_APP} smime -binary -sign -certfile {WWDR_PATH} -signer passcertificate.pem -inkey passkey.pem -in manifest.json -out signature -outform DER -passin pass:{KEY_PASSWORD}")
+    os_code = os.system(f"{OPENSSL_APP} smime -binary -sign -certfile {WWDR_PATH} -signer passcertificate.pem -inkey passkey.pem -in manifest.json -out signature -outform DER -passin pass:{KEY_PASSWORD}")
+    if os_code != 0:
+        print("Error creating signature")
+        return
 
     asset_files_to_delete = [
         "passkey.pem",
@@ -92,10 +120,11 @@ def main():
         for asset_file in asset_files:
             zip_file.write(asset_file)
 
-    os.system(f"open {PK_PASS_NAME}.pkpass")
+    os_code = os.system(f"open {PK_PASS_NAME}.pkpass")
+    if os_code != 0:
+        print("Error opening pass")
+        return
 
-    for asset_file in asset_files_to_delete:
-        os.remove(asset_file)
 
 
 def create_manifest_json(asset_path: str):
@@ -122,5 +151,6 @@ def create_pass_dict(pass_type_identifier: str, team_identifier: str):
     pass_dict_copy["passTypeIdentifier"] = pass_type_identifier
     pass_dict_copy["teamIdentifier"] = team_identifier
     return pass_dict_copy
+
 
 main()
